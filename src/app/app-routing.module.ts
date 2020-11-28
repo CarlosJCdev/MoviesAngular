37623a84db7d35218deacbd5c8e30778172eb1ce
CarlosJCdev <carlos4561059919@gmail.com>
2020-11-27 18:34:20 -0600

    ajuste de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PeliculaComponent } from './pages/pelicula/pelicula.component';
 
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'pelicula:/id', component: HomeComponent},
+  {path: 'pelicula/:id', component: PeliculaComponent},
   {path: 'buscar/:texto', component: BuscarComponent},
   {path: '**', redirectTo: '/home'}
 ];
