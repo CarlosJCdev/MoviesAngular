220f68dfae983b3b464616676f043143a9b300ae
CarlosJCdev <carlos4561059919@gmail.com>
2020-11-29 13:39:28 -0600

    En el componente peliculas-poster-grid, se llamo al input donde esta la llamada al API, para prescentar los datos desde ese componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PeliculaComponent } from '../pages/pelicula/pelicula.component';
 
 
 
 @NgModule({
   declarations:
-  [NavbarComponent, SlideshowComponent],
+  [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
   exports: [
     NavbarComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    PeliculasPosterGridComponent
   ],
   imports: [
     CommonModule,
