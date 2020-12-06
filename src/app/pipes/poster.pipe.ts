a0d75d32e66acc8dbf348f5c1055dfe658743e18
CarlosJCdev <carlos4561059919@gmail.com>
2020-12-06 08:51:20 -0600

    Se genero un pipe para controlar las imagenes que no estan disponibles en la API

--- src/app/pipes/poster.pipe.ts
+++ src/app/pipes/poster.pipe.ts
@@ -5,9 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PosterPipe implements PipeTransform {
 
+  /* Con este pipe controlamos las imagenes que no vienen con la API, entonces
+  si el valir es null, mostramos la imagen en assets de imagen no disponible
+  pero por el contrario si la imagen esta disponible la muestra */
   transform(poster: string): string {
-    console.log(poster);
-    return poster;
+
+    if (poster){
+      return `https://image.tmdb.org/t/p/w500${ poster }`;
+    } else{
+      return './assets/no-image.jpg';
+    }
+
   }
 
 }
